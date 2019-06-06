6b8aaa504f82e0c4962b03b326a426eab8f4686c
WINERED\akamine <ibg.akamine@gmail.com>
2019-06-06 19:14:42 +0900

    ルーティングとビューの追加

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Link::class, function (Faker $faker) {
+    return [
+      'title' => substr($faker->sentence(2), 0, -1),
+      'url' => $faker->url,
+      'description' => $faker->paragraph,
+    ];
+});
